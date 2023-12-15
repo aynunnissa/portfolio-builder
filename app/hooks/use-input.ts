364054a155716df63bc89871a058d47746ef12f8
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-12-15 19:38:36 +0700

    feat: add input hooks

--- app/hooks/use-input.ts
+++ app/hooks/use-input.ts
@@ -1,22 +1,33 @@
 import { useState } from 'react';
 
-const useInput = (validateValue: Function) => {
-  const [enteredValue, setEnteredValue] = useState<string>('');
+interface UseInputReturn {
+  value: string;
+  isValid: boolean;
+  hasError: boolean;
+  valueChangeHandler: (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => void;
+  valueChangeManual: (val: string) => void;
+  defaultValueHandler: (val: string) => void;
+  inputBlurHandler: (event: React.SyntheticEvent) => void;
+  reset: () => void;
+}
+
+const useInput = (validateValue: Function): UseInputReturn => {
+  const [value, setValue] = useState<string>('');
   const [isTouched, setIsTouched] = useState<boolean>(false);
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const isValid = validateValue(value);
+  const hasError = !isValid && isTouched;
 
-  const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEnteredValue(event.target.value);
+  const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
   };
 
   const valueChangeManual = (val: string) => {
-    setEnteredValue(val);
+    setValue(val);
   };
 
   const defaultValueHandler = (val: string) => {
-    setEnteredValue(val);
+    setValue(val);
   };
 
   const inputBlurHandler = (event: React.SyntheticEvent): void => {
@@ -24,20 +35,20 @@ const useInput = (validateValue: Function) => {
   };
 
   const reset = () => {
-    setEnteredValue('');
+    setValue('');
     setIsTouched(false);
   };
 
-  return [
-    enteredValue,
-    valueIsValid,
+  return {
+    value,
+    isValid,
     hasError,
     valueChangeHandler,
     valueChangeManual,
     defaultValueHandler,
     inputBlurHandler,
     reset,
-  ];
+  };
 };
 
 export default useInput;
