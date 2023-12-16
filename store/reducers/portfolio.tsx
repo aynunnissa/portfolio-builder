e8b4979d05e9e1c6847f42b70f485e8c2935b7d1
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2023-12-16 09:53:19 +0700

    feat: add object portfolio to handle changes

--- store/reducers/portfolio.tsx
+++ store/reducers/portfolio.tsx
@@ -1,29 +1,52 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { IPortfolio } from "@/types/user";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IPortfolio } from '@/types/user';
+
+interface IExistPortfolio {
+  [key: string]: Object;
+}
 
 interface IProductState {
-    portfolios: IPortfolio[]
+  existingPortfolios: IExistPortfolio;
+  portfolios: IPortfolio[];
+  totalChanged: number;
 }
 
 const initialState: IProductState = {
-    portfolios: []
-}
+  existingPortfolios: {},
+  portfolios: [],
+  totalChanged: 0,
+};
 
 const portfolioSlice = createSlice({
-    name: "products",
-    initialState,
-    reducers: {
-        loadPortfolio: (state, action: PayloadAction<IPortfolio>) => {
-            return {
-                ...state,
-                portfolios: state.portfolios
-            };
-        }
-    }
-})
+  name: 'products',
+  initialState,
+  reducers: {
+    loadPortfolio: (state, action: PayloadAction<IPortfolio[]>) => {
+      /**
+       * Create map object from existing portfolio for changes checking purpose
+       */
+      const objectPortfolios: IExistPortfolio = {};
+      action.payload.forEach((portfolio: IPortfolio) => {
+        const portID = portfolio.id;
+        const portObj = {
+          ...portfolio,
+          changed: false,
+        };
+
+        objectPortfolios[portID] = portObj;
+      });
+      return {
+        ...state,
+        portfolios: action.payload,
+        existingPortfolios: { ...objectPortfolios },
+      };
+    },
+  },
+});
 
 export const { loadPortfolio } = portfolioSlice.actions;
 
-export default portfolioSlice.reducer
+export default portfolioSlice.reducer;
 
-export const portfolioSelector = (state: { portfolioStore: IProductState }) => state.portfolioStore
+export const portfolioSelector = (state: { portfolioStore: IProductState }) =>
+  state.portfolioStore;
